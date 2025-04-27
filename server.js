8af27e8ef6ee215ac2730fe0bb0e66fa79b4593e
Dev-Rohan1 <mdrohanulhaquerohan368@gmail.com>
2025-04-27 20:06:55 +0600

    backend setup

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-import express from "express";
-import "dotenv/config";
 import bodyParser from "body-parser";
 import cors from "cors";
+import "dotenv/config";
+import express from "express";
 
 import connectDB from "./src/db/connectDB.js";
+import companyRoutes from "./src/routes/companyRoutes.js";
+import jobRoutes from "./src/routes/jobRoutes.js";
+import userRoutes from "./src/routes/userRoutes.js";
 import Cloudinary from "./src/utils/Cloudinary.js";
 
 const app = express();
@@ -15,6 +18,9 @@ connectDB();
 Cloudinary();
 
 app.get("/", (req, res) => res.send("api is working"));
+app.use("/user", userRoutes);
+app.use("/company", companyRoutes);
+app.use("/job", jobRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🌐Server is running on port ${PORT}`));
