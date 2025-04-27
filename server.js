4896eaa5a7d66d8411be34951e80fbb4c6baddc0
Dev-Rohan1 <mdrohanulhaquerohan368@gmail.com>
2025-04-27 19:08:32 +0600

    backend setup

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ import "dotenv/config";
 import bodyParser from "body-parser";
 import cors from "cors";
 
-// import connectDB from "./src/db/connectDB.js";
+import connectDB from "./src/db/connectDB.js";
 // import userRoutes from "./src/routes/userRoutes.js";
 // import companyRoutes from "./src/routes/companyRoutes.js";
 // import jobRoutes from "./src/routes/jobRoutes.js";
-// import Cloudinary from "./src/utils/cloudinary.js";
+import Cloudinary from "./src/utils/cloudinary.js";
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-// connectDB();
-// Cloudinary();
+connectDB();
+Cloudinary();
 
 app.get("/", (req, res) => res.send("api is working"));
 
